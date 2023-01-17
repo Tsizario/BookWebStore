62632e45d464381e506bb6baa244f4819b655513
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-01-18 01:51:45 +0200

    First commit

---
+++ src/BookWebStore/4. UI/BookWebStore.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookWebStore.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
