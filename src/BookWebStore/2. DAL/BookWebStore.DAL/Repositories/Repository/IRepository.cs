300d5e2b95419c7ef510b2c8e62fb57944ec8bbe
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-01-18 05:06:42 +0200

    Added implementation of DbRepository (correctly)

---
+++ src/BookWebStore/2. DAL/BookWebStore.DAL/Repositories/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace BookWebStore.DAL.Repositories.Abstractions
+{
+    public interface IDbRepository<T> where T : class
+    {
+        public IQueryable<T> AllItems { get; }
+
+        Task<IEnumerable<T>> GetAllItemsAsync(Expression<Func<T, bool>> filter = null, string includeProps = null);
+
+        Task<T> GetItemAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true, string includeProps = null);
+
+        Task<bool> AddItemAsync(T entity);
+
+        Task<bool> RemoveItemAsync(Guid id);
+
+        Task<bool> RemoveItemsAsync(IEnumerable<T> entities);
+    }
+}
---
+++ src/BookWebStore/2. DAL/BookWebStore.DAL/Repositories/Repository/Repository.cs
@@ -0,0 +1,112 @@
+﻿using BookWebStore.DAL.Repositories.Abstractions;
+using BookWebStore.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BookWebStore.DAL.Repositories.Repository
+{
+    public class DbRepository<T> : IDbRepository<T> 
+        where T : class, IDbEntity
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> _dbSet;        
+
+        public DbRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public IQueryable<T> AllItems => _dbSet;
+
+        public async Task<IEnumerable<T>> GetAllItemsAsync(Expression<Func<T, bool>> filter = null, string includeProps = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProps != null)
+            {
+                foreach (var item in includeProps.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetItemAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true, string includeProps = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProps != null)
+            {
+                foreach (var item in includeProps.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);    
+                }
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> AddItemAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+
+            return await SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveItemAsync(Guid id)
+        {
+            T candidate = await AllItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (candidate != null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(candidate);
+
+            return await SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveItemsAsync(IEnumerable<T> entities)
+        {
+            if (entities == null)
+            {
+                return false;
+            }
+
+            _dbSet.RemoveRange(entities);
+
+            return await SaveChangesAsync() > 0;
+        }
+
+        private async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+    }
+}
