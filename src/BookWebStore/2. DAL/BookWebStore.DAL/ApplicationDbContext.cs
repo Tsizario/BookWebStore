941c81bd2940c3034d5c6a6fc09a7b484c32ef1a
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-01-18 02:41:41 +0200

    Added data access layer

---
+++ src/BookWebStore/2. DAL/BookWebStore.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWebStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebStore.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {           
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ src/BookWebStore/2. DAL/BookWebStore.DAL/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookWebStore.Domain.Entities;
+
+namespace BookWebStore.DAL.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllEntitiesAsync();
+
+        Task<Category> GetByIdAsync(Guid? id);
+
+        Task<bool> AddAsync(Category item);
+
+        Task<bool> UpdateAsync(Category item);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
