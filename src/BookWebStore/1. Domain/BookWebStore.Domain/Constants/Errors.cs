81e2829ff19caa06884a1011b933fa1b8b497962
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-01-18 02:14:27 +0200

    Added first constants, entities and abstractions

---
+++ src/BookWebStore/1. Domain/BookWebStore.Domain/Constants/Errors.cs
@@ -0,0 +1,10 @@
+﻿namespace BookWebStore.Domain.Constants
+{
+    public class Errors
+    {
+        public const string CategoriesNotFound = "Categories not found";
+        public const string CategoryNotFound = "Category not found";
+        public const string CategoryAddingError = "Error while adding category";
+        public const string CategoriesAddingError = "Error while adding categories";
+    }
+}
